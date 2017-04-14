15d9014f7882855b3fdbe08f6a6794bd61c00be9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-04-14 15:44:47 -0700

    controllers upd

--- server/controllers/dashboard-controller.js
+++ server/controllers/dashboard-controller.js
@@ -80,32 +80,6 @@ exports.getProfile = function(req, res, next){
   
 };
 
-// @TODO create controller for WHOIS settings or rename and use getProfile.
-
-//prev version
-exports.getProfile2 = function(req, res, next){
-
-  var form  = {},
-      error = null,
-      formFlash  = req.flash('form'),
-      errorFlash = req.flash('error');
-
-  if (formFlash.length) {
-    form.email = formFlash[0].email;
-  }
-  
-  if (errorFlash.length) {
-    error = errorFlash[0];
-  }
-
-  res.render(req.render, {
-    user: req.user, 
-    form: form, 
-    error: error, 
-    plans: plans
-  });
-  
-};
 
 //@TODO finish and test
 exports.getCancelMyAccountAction = function (req, res, next){
