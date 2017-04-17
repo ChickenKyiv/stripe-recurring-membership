5b8b6ebe9571f6f5691c0a6a21658d4d75631746
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-04-17 05:57:46 -0700

    routes update

--- server/routes/registration.js
+++ server/routes/registration.js
@@ -32,54 +32,73 @@ module.exports    = function (app, passport) {
   // );
 
 
-  app.get('/signup2',
-    setRedirect({auth: '/dashboard'}),
-    isUnauthenticated,
-    setRender('signup2'),
-    registrations.getSignup2
-  );
+
+  app.routes('/signup2')
+     .all(setRedirect({auth: '/dashboard', success: '/signup2-1', failure: '/signup2'}))
+     .all(isUnauthenticated)
+     .get(setRender('signup2'), registrations.getSignup2)
+     .post(registrations.postSignup2);
 
 
-  app.post('/signup2',
-    setRedirect({auth: '/dashboard', success: '/signup2-1', failure: '/signup2'}),
-    // setRedirect({auth: '/dashboard', success: '/dashboard', failure: '/signup2'}),
-    isUnauthenticated,
-    registrations.postSignup2
-  );
+  // app.get('/signup2',
+  //   setRedirect({auth: '/dashboard'}),
+  //   isUnauthenticated,
+  //   setRender('signup2'),
+  //   registrations.getSignup2
+  // );
 
+
+  // app.post('/signup2',
+  //   setRedirect({auth: '/dashboard', success: '/signup2-1', failure: '/signup2'}),
+  //   // setRedirect({auth: '/dashboard', success: '/dashboard', failure: '/signup2'}),
+  //   isUnauthenticated,
+  //   registrations.postSignup2
+  // );
+
+
+
+  app.routes('signupshort')
+     .all(setRedirect({auth: '/dashboard', success: '/dashboard', failure: '/signup2'}))
+     .all(isUnauthenticated)
+     .get(setRender('signup2-1'), registrations.getSignup2)
+     .post(registrations.postSignupFirstTime);
 
 //user registration before purchase
-  app.post('/signupshort',
-    setRedirect({auth: '/dashboard', success: '/dashboard', failure: '/signup2'}),
-    isUnauthenticated,
-    registrations.postSignupFirstTime
-  );
+  // app.post('/signupshort',
+  //   setRedirect({auth: '/dashboard', success: '/dashboard', failure: '/signup2'}),
+  //   isUnauthenticated,
+  //   registrations.postSignupFirstTime
+  // );
 
 
-  app.get('/signup2-1',
-    setRedirect({auth: '/dashboard'}),
-    isUnauthenticated,
-    setRender('signup2-1'),
-    registrations.getSignup2
-  );
+  // app.get('/signup2-1',
+  //   setRedirect({auth: '/dashboard'}),
+  //   isUnauthenticated,
+  //   setRender('signup2-1'),
+  //   registrations.getSignup2
+  // );
 
-
+  app.routes('/whois')
+     .all(setRedirect({ auth: '/', success: '/profile', failure: '/profile' }))
+     .all(isAuthenticated)
+     .get(setRender('dashboard/whois-settings'), dashboard.getWhoisForm)
+     .post(users.postWhois);
 
   //display whois form 
   //@TODO change URL name
-  app.get('/whois',
-    setRender('dashboard/whois-settings'),
-    setRedirect({auth: '/'}),
-    isAuthenticated,
-    dashboard.getWhoisForm
-  );
+  // app.get('/whois',
+  //   setRender('dashboard/whois-settings'),
+  //   setRedirect({auth: '/'}),
+  //   isAuthenticated,
+  //   dashboard.getWhoisForm
+  // );
 
 //@TODO replace redirect rules
-  app.post('/whois-settings',
-    setRedirect({ auth: '/', success: '/profile', failure: '/profile' }),
-    isAuthenticated,
-    users.postWhois
-  );
+  // app.post('/whois-settings',
+  //   setRedirect({ auth: '/', success: '/profile', failure: '/profile' }),
+  //   isAuthenticated,
+  //   users.postWhois
+  // );
 
 
 
