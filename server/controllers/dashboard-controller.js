9a6033f273af4381133520fc9b0cb7e3d6ce2371
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-04-28 01:14:50 -0700

    profile page

--- server/controllers/dashboard-controller.js
+++ server/controllers/dashboard-controller.js
@@ -54,7 +54,7 @@ exports.getBilling = function(req, res, next){
 
 //dashboard/profile
 exports.getProfile = function (req, res, next){
-console.log('123');
+// console.log('123');
   var form       = {},
       error      = null,
       formFlash  = req.flash('form'),
